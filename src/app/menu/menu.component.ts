eb16b0f06939ccdc1c4b78c35d8f8a9285d1f349
jalili <mehdi@jalili.com>
2019-08-26 08:41:45 +0200

    fix navigation bar

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+
 
 @Component({
   selector: 'app-menu',
@@ -11,13 +14,24 @@ export class MenuComponent implements OnInit {
   isNavbarCollapsed=true;
   activeName="home";
 
-  constructor(private router:Router) { }
+  constructor(private router:Router) { 
+    this.detectPath();
+  }
 
   ngOnInit() {
   }
 
+  //detect url changes and active proper link
+  detectPath(){
+    const event$=this.router.events.pipe(
+      filter(event=> event instanceof NavigationEnd));
+    event$.subscribe((val:NavigationEnd)=> {
+      this.activeName=val.urlAfterRedirects.split('/')[1];
+      if(this.activeName==='') this.activeName='home';
+    });
+  }
+
   linkClick(name:string){
-    this.activeName=name;
     this.isNavbarCollapsed=true;
     this.router.navigate([name]);
   }
