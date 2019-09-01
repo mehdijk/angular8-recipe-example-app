3a11bf9b4ce28fc147410452f376b53049fa0919
jalili <mehdi@jalili.com>
2019-09-01 13:29:33 +0200

    mock data

--- src/app/service/recipe.service.ts
+++ src/app/service/recipe.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Output, EventEmitter } from '@angular/core';
 import { NgxIndexedDB } from 'ngx-indexed-db';
-import { Observable, of,from } from 'rxjs';
+import { of,from, observable } from 'rxjs';
 import { Recipe } from '../model/recipe.model';
 import { error } from '@angular/compiler/src/util';
 
@@ -22,34 +22,27 @@ export class RecipeService {
    }
 
   openDatabase():Promise<any>{
+    // this.myDb.dbWrapper.validateStoreName(this.storeName)
     return this.myDb.openDatabase(1);
   }
 
   createDb(){
     this.myDb.openDatabase(1, evt => {
       let objectStore = evt.currentTarget.result.createObjectStore(this.storeName,
-      { keyPath: 'id', autoIncrement: true });
-      objectStore.createIndex('name', 'name', { unique: true });
-      objectStore.createIndex('direction', 'direction', { unique: false });
-      objectStore.createIndex('imageUrl', 'imageUrl', { unique: false });
-      objectStore.createIndex('thumbImageUrl', 'thumbImageUrl', { unique: false });
-      objectStore.createIndex('cookingTime', 'cookingTime', { unique: false });
-      objectStore.createIndex('serves', 'serves', { unique: false });
-        }).then(
-        x=>console.log("Database is created!")
-        ).catch(error=>console.warn('Error in creating database: ' + error));
+       { keyPath: 'id', autoIncrement: true });
+        objectStore.createIndex('name', 'name', { unique: true });
+        objectStore.createIndex('direction', 'direction', { unique: false });
+        objectStore.createIndex('imageUrl', 'imageUrl', { unique: false });
+        objectStore.createIndex('thumbImageUrl', 'thumbImageUrl', { unique: false });
+        objectStore.createIndex('cookingTime', 'cookingTime', { unique: false });
+        objectStore.createIndex('serves', 'serves', { unique: false });
+        this.mockRecipes.forEach((recipe)=>this.addRecipe(recipe));
+        });
   }
 
   deleteDb(){
     this.openDatabase().then(x=>
-    this.myDb.deleteDatabase().then(
-      () => {
-          console.log('Database deleted successfully');
-      },
-      error => {
-          console.log(error);
-      }
-  ));
+    this.myDb.deleteDatabase());
   }
 
   getAll(){
@@ -86,4 +79,38 @@ export class RecipeService {
       this.myDb.update(this.storeName,recipe);
     });
   }
+
+
+   
+
+  //mocks
+  private mockRecipes:Recipe[]=[
+    {
+    name:'Pizza Margherita',
+    direction:'Preheat the wood-fired pizza oven at high temperature for 4 hours before cooking.Alternatively, a refractory stone can be used, preheated in the domestic oven. It is preferable to heat it for an hour, at 260 ° C, before baking. '+
+              'Put the tomatoes in a blender and treat them until they blend together. Season with salt. Sprinkle the pizza scoop with flour. With a rolling pin, spread the dough until you get a thin circle of 10 inches. Using a spoon, put ¼ of the sauce on the dough and distribute, leaving a 1-inch edge. Cover the sauce with ¼ of mozzarella.',
+    imageUrl:'http://www.fnstatic.it/images/content/recipe/ccev103-margherita-pizza-s4x3-jpg_3.jpeg',
+    thumbImageUrl:'http://www.fnstatic.it/images/small/recipe/ccev103-margherita-pizza-s4x3-jpg_3.jpeg',
+    cookingTime:30,
+    serves:2
+    },
+    {
+      name:'Lasagne Bolognese',
+      direction:'Add the first layer of lasagna and in order, cover with ragù, about 3 ladlefuls of béchamel and a generous sprinkling of Parmesan. Repeat the same procedure until you reach the top of the pan. Make sure the lasagna is immersed in the sauce.'+
+                'When you finish putting the ingredients in layers, cover the lasagna with a final ladle of ragù and a little béchamel, add a few strips of butter and finish with a sprinkling of Parmesan.',
+      imageUrl:'http://www.fnstatic.it/images/content/recipe/ccev108-lasagne-alla-bolognese-s4x3-jpg_3.jpeg',
+      thumbImageUrl:'http://www.fnstatic.it/images/small/recipe/ccev108-lasagne-alla-bolognese-s4x3-jpg_3.jpeg',
+      cookingTime:180,
+      serves:4
+      },
+      {
+        name:'A food without image',
+        direction:'This is the directions for cooking. A part of direction is shown here and the user can read the complete description in the detail page. If the image or thumbnail is not specified or unavailable, a default image will be shown.',
+        imageUrl:'',
+        thumbImageUrl:'a wrong address',
+        cookingTime:180,
+        serves:4
+        },
+    
+  ];
 }
